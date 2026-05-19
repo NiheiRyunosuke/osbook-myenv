4eba5192745fab10e82809df2c158cd7c25b478d
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-05-19 23:13:48 +0900

    Newline関数を実装

--- mikanos/kernel/console.cpp
+++ mikanos/kernel/console.cpp
@@ -16,3 +16,21 @@ void Console::PutString(const char* s) {
     ++s;
   }
 }
+
+void Console::Newline() {
+  cursor_column_ = 0;
+  if (cursor_row_ < kRows - 1) {
+    ++cursor_row_;
+  } else {
+    for (int y = 0; y< 16 * kRows; ++ y) {
+      for int x = 0; x < 8 * kColumns; ++x {
+        writer_.Write(x, y, bg_color_);
+        }
+    }
+    for (int row = 0; row < kRows -1 ; ++row) {
+      memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
+      WriteString(writer_, 0, 16 * row, buffer_[row], fg_color_);
+    }
+    memset(buffer_[kRows -1], 0, kColumns + 1);
+  } 
+}
