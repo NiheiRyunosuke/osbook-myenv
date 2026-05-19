95d2137ab33a9805950ce99fe48bc605b8084648
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-05-19 23:29:36 +0900

    Console オブジェクトを置くための実体用メモリと、そのオブジェクトを指すポインタを追記

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -4,6 +4,7 @@
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
 #include "font.hpp"
+#include "console.hpp"
 
 void* operator new(size_t size, void* buf) {
   return buf;
@@ -14,6 +15,9 @@ void operator delete(void* obj) noexcept{
 
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter* pixel_writer;
+
+char console_buf[sizeof(Console)];
+Console* console;
 
 extern "C" __attribute__((ms_abi))
 void KernelMain(const FrameBufferConfig& frame_buffer_config) {
@@ -35,17 +39,29 @@ void KernelMain(const FrameBufferConfig& frame_buffer_config) {
       pixel_writer->Write(x, y, {255, 255, 255});
     }
   }
-  for (int x = 100; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      pixel_writer->Write(x, y, {0, 255, 0});
-    }
-  }
+  // for (int x = 100; x < 200; ++x) {
+  //   for (int y = 0; y < 100; ++y) {
+  //     pixel_writer->Write(x, y, {0, 255, 0});
+  //   }
+  // }
 
-  int i = 0;
-  for (char c = '!'; c <= '~'; ++c, ++i) {
-    WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
+  // int i = 0;
+  // for (char c = '!'; c <= '~'; ++c, ++i) {
+  //   WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
+  // }
+  // WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
+
+  // char buf[128];
+  // sprintf(buf, "1 + 2 = %d", 1 + 2);
+  // WriteString(*pixel_writer, 0, 82, buf, {0, 0, 0});
+
+  console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+
+  char buf[32];
+  for (int i = 0; i < 30; ++i) {
+    sprintf(buf, "line %d\n", i);
+    console->PutString(buf);
   }
-  WriteString(*pixel_writer, 0, 66, "Hello, world!", {0, 0, 255});
 
   while (1) {
     __asm__("hlt");
