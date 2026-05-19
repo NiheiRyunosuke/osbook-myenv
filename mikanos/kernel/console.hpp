37523e85fd8c5d768173d2a9d3232ce721d385ab
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-05-19 22:28:15 +0900

    各行の最後に文字列終端 '\0' を入れて、その行をC文字列として安全に扱えるように改修

--- mikanos/kernel/console.hpp
+++ mikanos/kernel/console.hpp
@@ -15,6 +15,6 @@ class Console {
 
       PixelWriter& writer_;
       const PixelColor fg_color_, bg_color_;
-      char buffer_[kRows][kColumns];
+      char buffer_[kRows][kColumns + 1];
       int cursor_row_, cursor_column_;
 };
