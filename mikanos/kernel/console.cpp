25a0b84e1772facd79375eb05a311670dfa58e80
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-05-19 22:47:39 +0900

    コンソールの現在位置を管理しながら、文字列を画面と内部バッファの両方に書き込む関数

--- mikanos/kernel/console.cpp
+++ mikanos/kernel/console.cpp
@@ -3,3 +3,16 @@ Console::Console(PixelWriter& writer,
     : writer_{writer}, fg_color_{fg_color}, bg_color_{bg_color},
       buffer_{}, cursor_row_{0}, cursor_column_{0} {
       }
+
+void Console::PutString(const char* s) {
+  while (*s) {
+    if (*s == '\n') {
+      Newline();
+    } else if (cursor_column_ < kColumns -1 ) {
+      WriteAscii(writer_, 8 * cursor_column_, 16 * cursor_row_, *s, fg_color_);
+      buffer_[cursor_row_][cursor_column_] = *s;
+      ++cursor_column_;
+    }
+    ++s;
+  }
+}
