f0ece959715ea34530bd63cc5ad7b9b1f07ce065
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-05-24 18:32:23 +0900

    グローバル変数をヘッダファイルに直接定義して共有したいので、inline 変数として定義

--- mikanos/kernel/pci.hpp
+++ mikanos/kernel/pci.hpp
@@ -2,3 +2,14 @@
 const uint16_t kConfigAddress = 0x0cf8;
 /** @brief CONFIG_DATA レジスタのIO ポートアドレス */
 const uint16_t kConfigData = 0x0cfc;
+
+/** @brief ScanAllBus() により発見された PCI デバイスの一覧 */
+inline std::array<Device, 32> device;
+/** @brief devices の有効な要素の数 */
+inline int num_device;
+/** @brief PCI デバイスをすべて探索し devices に格納する
+ * 
+ * バス 0 から再帰的に PCI デバイスを探索し、devices の先頭から詰めて書き込む.
+ * 発見したデバイスの数を num_devices に設定する
+ */
+Error ScanAllBus();
