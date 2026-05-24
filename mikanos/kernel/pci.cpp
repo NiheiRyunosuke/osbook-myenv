6a665630ccdf9bb247bc980250ea2cabdee9082f
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-05-24 16:47:30 +0900

    MakeAdressの関数実装

--- mikanos/kernel/pci.cpp
+++ mikanos/kernel/pci.cpp
@@ -7,3 +7,15 @@
 #include "pci.hpp"
 
 /** @brief CONFIG_ADDRESS 用の 32 ビット整数を生成する */
+uint32_t MakeAddress(uint8_t bus, uint8_t device,
+                    uint8_t function, uint8_t reg_addr){
+  auto shl = [](uint32_t x, unsigned int bits) {
+    return x << bits;
+  };
+
+  return shl(1, 31)  // enable bit
+      | shl(bus, 16)
+      | shl(device, 11)
+      | shl(function, 8)
+      | (reg_addr & 0xfcu);
+    }
