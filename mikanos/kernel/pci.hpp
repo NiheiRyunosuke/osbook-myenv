3ef8e026a6b411a9083febf07f44366c9a1e50e6
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-05-28 15:38:58 +0900

    pci 名前空間・Device・Error・PCI 関数宣言

--- mikanos/kernel/pci.hpp
+++ mikanos/kernel/pci.hpp
@@ -1,15 +1,64 @@
-/** @brief CONFIG_ADDRESS レジスタのIO ポートアドレス */
+#pragma once
+
+#include <array>
+#include <stdint.h>
+
+class Error {
+ public:
+  enum Code {
+    kSuccess,
+    kFull,
+  };
+
+  Error(Code code) : code_{code} {
+  }
+
+  operator bool() const {
+    return code_ != kSuccess;
+  }
+
+  const char* Name() const {
+    switch (code_) {
+      case kSuccess:
+        return "Success";
+      case kFull:
+        return "Full";
+    }
+    return "Unknown";
+  }
+
+ private:
+  Code code_;
+};
+
+namespace pci {
+
+/** @brief CONFIG_ADDRESS register I/O port address */
 const uint16_t kConfigAddress = 0x0cf8;
-/** @brief CONFIG_DATA レジスタのIO ポートアドレス */
+/** @brief CONFIG_DATA register I/O port address */
 const uint16_t kConfigData = 0x0cfc;
 
-/** @brief ScanAllBus() により発見された PCI デバイスの一覧 */
-inline std::array<Device, 32> device;
-/** @brief devices の有効な要素の数 */
+struct Device {
+  uint8_t bus, device, function, header_type;
+};
+
+/** @brief List of PCI devices found by ScanAllBus(). */
+inline std::array<Device, 32> devices;
+/** @brief Number of valid elements in devices. */
 inline int num_device;
-/** @brief PCI デバイスをすべて探索し devices に格納する
- * 
- * バス 0 から再帰的に PCI デバイスを探索し、devices の先頭から詰めて書き込む.
- * 発見したデバイスの数を num_devices に設定する
- */
+
+void WriteAddress(uint32_t address);
+void WriteData(uint32_t value);
+uint32_t ReadData();
+
+uint16_t ReadVendorId(uint8_t bus, uint8_t device, uint8_t function);
+uint16_t ReadDeviceId(uint8_t bus, uint8_t device, uint8_t function);
+uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function);
+uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function);
+uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function);
+bool IsSingleFunctionDevice(uint8_t header_type);
+
+/** @brief Scan all PCI devices and store them in devices. */
 Error ScanAllBus();
+
+}
