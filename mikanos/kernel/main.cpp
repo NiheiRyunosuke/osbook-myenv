216af3c2f9b2a0b16c47fab1def83e01ce9c7bc5
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-01 19:22:55 +0900

    リスト6.19 BAR0のレジスタの読み取りを実装

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -139,8 +139,14 @@ void KernelMain(const FrameBufferConfig& frame_buffer_config) {
 
   if (xhc_dev) {
     Log(kInfo, "xHC has been found: %d.%d.%d\n",
-        xhc_dev->bus, xhc_dev_dev->device, xhc_dev->function);
+        xhc_dev->bus, xhc_dev->device, xhc_dev->function);
   }
+
+  const WithError<uint64_t> xhc_bar = pci::ReadBar(*xhc_dev, 0);
+  Log(kDebug, "ReadBar: %s\n", xhc_bar.error.Name());
+  const uint64_t xhc_mmio_base = xhc_bar.value & ~static_cast<uint64_t>(0xf);
+  Log(kDebug, "xHC mmio_base = %08lx\n", xhc_mmio_base);
+
 
   while (1) {
     __asm__("hlt");
