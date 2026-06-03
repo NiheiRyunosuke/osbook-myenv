3427b8a7dbf0a62cdf35cc97cc6137c21331942f
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-03 19:06:22 +0900

    EraseMouseCursorを記述+無名名前空間でガード

--- mikanos/kernel/mouse.cpp
+++ mikanos/kernel/mouse.cpp
@@ -1,7 +1,7 @@
 #include "mouse.hpp"
 #include "graphics.hpp"
 
-// namespace {
+namespace {
 const int kMouseCursorWidth = 15;
 const int kMouseCursorHeight = 24;
 const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
@@ -41,6 +41,18 @@ void DrawMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position) {
       }
     }
   }
+}
+
+void EraseMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position,
+                    PixelColor erase_color) {
+  for (int dy = 0; dy < kMouseCursorHeight; ++dy) {
+    for (int dx = 0; dx < kMouseCursorWidth; ++ dx) {
+      if (mouse_cursor_shape[dy][dx] != ' ') {
+        pixel_writer->Write(position.x + dx, position.y + dy, erase_color);
+      }
+    }
+  }
+}
 }
 
 MouseCursor::MouseCursor(PixelWriter* writer, PixelColor erase_color,
