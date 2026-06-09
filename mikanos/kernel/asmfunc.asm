a3e3f8acf7817cbf7e54b99dec291aa216d51735
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-09 17:37:10 +0900

    7.7

--- mikanos/kernel/asmfunc.asm
+++ mikanos/kernel/asmfunc.asm
@@ -17,3 +17,15 @@ IoIn32:
     mov dx, di    ; dx = addr
     in eax, dx
     ret
+
+global LoadIDT  ; void LoadIDT(uint16_t limit, uint64_t offset);
+LoadIDT:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 10
+    mov [rsp], di  ; limit
+    mov [rsp + 2], rsi  ; offset
+    lidt [rsp]
+    mov rsp, rbp
+    pop rbp
+    ret
