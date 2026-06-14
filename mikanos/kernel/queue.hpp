1d95bb906bc165f7f84c72080ff0f036734a8bb3
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-14 20:45:08 +0900

    7.10

---
+++ mikanos/kernel/queue.hpp
@@ -0,0 +1,22 @@
+template <typename T>
+class ArrayQueue {
+  public:
+    template <size_t N>
+    ArrayQueue(std::array<t, N>& buf);
+    ArrayQueue(T* buf, size_t size);
+    Error Push(const T& value);
+    Error Pop();
+    size_t Count() const;
+    size_t Capacity() const;
+    const T& Front() const;
+
+  private:
+    T* data_;
+    size_t read_pos_, write_pos_, count_;
+    /* 
+     * read_pos_ points to an element to be read.
+     * write_pos_ points to a blank position.
+     * count_ is the number of elements available.
+     */
+  const size_t capacity_;
+}
