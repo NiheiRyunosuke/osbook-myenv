be2f556abfa7e5334c4f9ebd6ded8b12dbb4ce1a
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-19 16:44:48 +0900

    typo修正

--- mikanos/kernel/memory_manager.cpp
+++ mikanos/kernel/memory_manager.cpp
@@ -1,4 +1,4 @@
-void BitamapMemoryManager::MarkAllocated(FrameID start_frame, size_t num_frames) {
+void BitmapMemoryManager::MarkAllocated(FrameID start_frame, size_t num_frames) {
   for (size_t i = 0; i < num_frames; ++i) {
     SetBit(FrameID{start_frame.ID() + i}, true);
   }
