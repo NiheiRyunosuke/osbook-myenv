1b467757047116aada7ead289cfb629f0e3fc523
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-19 19:04:47 +0900

    9.1

--- mikanos/kernel/newlib_support.c
+++ mikanos/kernel/newlib_support.c
@@ -1,5 +1,66 @@
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <stddef.h>
 
 caddr_t sbrk(int incr) {
   return NULL;
+}
+
+void _exit(int status) {
+  while (1) {
+    __asm__("hlt");
+  }
+}
+
+int close(int fd) {
+  return -1;
+}
+
+int fstat(int fd, struct stat* st) {
+  st->st_mode = S_IFCHR;
+  return 0;
+}
+
+int getpid(void) {
+  return 1;
+}
+
+int isatty(int fd) {
+  return 1;
+}
+
+int kill(int pid, int sig) {
+  errno = EINVAL;
+  return -1;
+}
+
+off_t lseek(int fd, off_t offset, int whence) {
+  return 0;
+}
+
+ssize_t read(int fd, void* buf, size_t count) {
+  return 0;
+}
+
+ssize_t write(int fd, const void* buf, size_t count) {
+  return count;
+}
+
+int posix_memalign(void** memptr, size_t alignment, size_t size) {
+  *memptr = NULL;
+  return ENOMEM;
+}
+
+caddr_t program_break, program_break_end;
+
+caddr_t sbrk(int incr) {
+  if (program_break == 0 || program_break + incr >= program_break_end) {
+    errno = ENOMEM;
+    return (caddr_t)-1;
+  }
+
+  caddr_t prev_break = program_break;
+  program_break += incr;
+  return prev_break;
 }
