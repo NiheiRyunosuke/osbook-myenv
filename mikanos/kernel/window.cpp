3e8ebb9008d84e9f04bd162d70232a6b40f9e80e
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-22 20:10:10 +0900

    9.5

---
+++ mikanos/kernel/window.cpp
@@ -0,0 +1,8 @@
+#include "window.hpp"
+
+Window::Window(int width, int height) : width_{width}, height_{height} {
+  data_.resize(height);
+  for (int y = 0; y < height; ++y) {
+    data_[y].resize(width);
+  }
+}
