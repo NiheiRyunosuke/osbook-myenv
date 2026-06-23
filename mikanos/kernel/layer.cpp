68569ae76587b1022d4015e3af92c479793d21c0
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-23 15:06:28 +0900

    9.10

---
+++ mikanos/kernel/layer.cpp
@@ -0,0 +1,11 @@
+#include "layer.hpp"
+
+Layer& Layer::Move(Vector2D<int> pos) {
+  pos_ = pos;
+  return *this;
+}
+
+Layer& Layer::MoveRelative(Vector2D<int> pos_diff) {
+  pos_ += pos_diff;
+  return *this;
+}
