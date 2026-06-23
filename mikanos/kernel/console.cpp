351647436c6b99cd4393fc1ab68ed43081dea677
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-23 21:30:21 +0900

    9.24

--- mikanos/kernel/console.cpp
+++ mikanos/kernel/console.cpp
@@ -12,11 +12,14 @@ void Console::PutString(const char* s) {
     if (*s == '\n') {
       Newline();
     } else if (cursor_column_ < kColumns -1 ) {
-      WriteAscii(writer_, 8 * cursor_column_, 16 * cursor_row_, *s, fg_color_);
+      WriteAscii(*writer_, 8 * cursor_column_, 16 * cursor_row_, *s, fg_color_);
       buffer_[cursor_row_][cursor_column_] = *s;
       ++cursor_column_;
     }
     ++s;
+  }
+  if (layer_manager) {
+    layer_manager->Draw();
   }
 }
 
