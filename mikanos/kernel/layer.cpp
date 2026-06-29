6b154531988cff71be1dd74c95445e9f1330119e
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-29 22:23:52 +0900

    修正

--- mikanos/kernel/layer.cpp
+++ mikanos/kernel/layer.cpp
@@ -33,8 +33,8 @@ void Layer::DrawTo(FrameBuffer& screen) const {
   }
 }
 
-void LayerManager::SetWriter(PixelWriter* writer) {
-  writer_ = writer;
+void LayerManager::SetWriter(FrameBuffer* screen) {
+  screen_ = screen;
 }
 
 Layer& LayerManager::NewLayer() {
