e2df0c51f75514e3a5bc486e320d99559482065b
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-06-30 19:25:16 +0900

    9.43

--- mikanos/kernel/console.cpp
+++ mikanos/kernel/console.cpp
@@ -29,18 +29,20 @@ void Console::Newline() {
   cursor_column_ = 0;
   if (cursor_row_ < kRows - 1) {
     ++cursor_row_;
+    return;
+  }
+  if (window_) {
+    Rectangle<int> move_src{{0, 16}, {8* kColumns, 16 * (kRows - 1)}};
+    window_->Move({0, 0}, move_src);
+    FillRectangle(*writer_, {0, 16 * (kRows -1)}, {8 * kColumns, 16}, bg_color_); //移動して空いた最終行を塗りつぶす
   } else {
-    for (int y = 0; y< 16 * kRows; ++ y) {
-      for (int x = 0; x < 8 * kColumns; ++x) {
-        writer_->Write(Vector2D<int>{x, y}, bg_color_);
-        }
-    }
-    for (int row = 0; row < kRows -1 ; ++row) {
+    FillRectangle(*writer_, {0, 0}, {8 * kColumns, 16 * kRows}, bg_color_);
+    for (int row = 0; row < kRows -1; ++row) {
       memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
-      WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
+      WriteString(*writer_, Vector2D<int>{0, 16* row}, buffer_[row], fg_color_);
     }
     memset(buffer_[kRows -1], 0, kColumns + 1);
-  } 
+  }
 }
 
 void Console::SetWriter(PixelWriter* writer) {
