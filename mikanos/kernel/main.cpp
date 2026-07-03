43697a1f0fc7a54dd3c047d92a070be1da5dc353
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-04 05:12:58 +0900

    10.8

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -264,10 +264,8 @@ extern "C" void KernelMainNewStack(
   mouse_position = {200, 200};
 
   auto main_window = std::make_shared<Window>(
-      160, 68, frame_buffer_config.pixel_format);
+      160, 52, frame_buffer_config.pixel_format);
   DrawWindow(*main_window->Writer(), "Hello Window");
-  WriteString(*main_window->Writer(), {24, 28}, "Welcome to", {0, 0, 0});
-  WriteString(*main_window->Writer(), {24, 44}, "MikanOS world!", {0, 0, 0});
 
   FrameBuffer screen;
   if (auto err = screen.Initialize(frame_buffer_config)) {
