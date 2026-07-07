a0e5e52af7e5d54bf5d1aba6fddeb64c238b8d06
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-07 13:34:34 +0900

    void MouseObserverを著者合わせ

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -78,7 +78,6 @@ void MouseObserver(int8_t displacement_x, int8_t displacement_y) {
   mouse_position = ElementMax(newpos, {0, 0});
 
   layer_manager->Move(mouse_layer_id, mouse_position);
-  layer_manager->Draw();
 }
 
 char memory_manager_buf[sizeof(BitmapMemoryManager)];
