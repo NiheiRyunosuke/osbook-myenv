dbd080f96454d355fc65c1e18caf7c9a847a525e
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-14 17:28:10 +0900

    11.1

--- mikanos/kernel/console.cpp
+++ mikanos/kernel/console.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file console.cpp
+ *
+ * コンソール描画のプログラムを集めたファイル．
+ */
+
 #include "console.hpp"
 
 #include <cstring>
@@ -5,15 +11,15 @@
 #include "layer.hpp"
 
 Console::Console(const PixelColor& fg_color, const PixelColor& bg_color)
-    : writer_{nullptr}, fg_color_{fg_color}, bg_color_{bg_color},
-      buffer_{}, cursor_row_{0}, cursor_column_{0} {
+    : writer_{nullptr}, window_{}, fg_color_{fg_color}, bg_color_{bg_color},
+      buffer_{}, cursor_row_{0}, cursor_column_{0}, layer_id_{0} {
 }
 
 void Console::PutString(const char* s) {
   while (*s) {
     if (*s == '\n') {
       Newline();
-    } else if (cursor_column_ < kColumns -1 ) {
+    } else if (cursor_column_ < kColumns - 1) {
       WriteAscii(*writer_, Vector2D<int>{8 * cursor_column_, 16 * cursor_row_}, *s, fg_color_);
       buffer_[cursor_row_][cursor_column_] = *s;
       ++cursor_column_;
@@ -22,26 +28,6 @@ void Console::PutString(const char* s) {
   }
   if (layer_manager) {
     layer_manager->Draw(layer_id_);
-  }
-}
-
-void Console::Newline() {
-  cursor_column_ = 0;
-  if (cursor_row_ < kRows - 1) {
-    ++cursor_row_;
-    return;
-  }
-  if (window_) {
-    Rectangle<int> move_src{{0, 16}, {8 * kColumns, 16 * (kRows - 1)}};
-    window_->Move({0, 0}, move_src);
-    FillRectangle(*writer_, {0, 16 * (kRows -1)}, {8 * kColumns, 16}, bg_color_); //移動して空いた最終行を塗りつぶす
-  } else {
-    FillRectangle(*writer_, {0, 0}, {8 * kColumns, 16 * kRows}, bg_color_);
-    for (int row = 0; row < kRows -1; ++row) {
-      memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
-      WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
-    }
-    memset(buffer_[kRows -1], 0, kColumns + 1);
   }
 }
 
@@ -54,12 +40,6 @@ void Console::SetWriter(PixelWriter* writer) {
   Refresh();
 }
 
-void Console::Refresh() {
-  for (int row = 0; row < kRows; ++row) {
-    WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
-  }
-}
-
 void Console::SetWindow(const std::shared_ptr<Window>& window) {
   if (window == window_) {
     return;
@@ -67,4 +47,53 @@ void Console::SetWindow(const std::shared_ptr<Window>& window) {
   window_ = window;
   writer_ = window->Writer();
   Refresh();
+}
+
+void Console::SetLayerID(unsigned int layer_id) {
+  layer_id_ = layer_id;
+}
+
+unsigned int Console::LayerID() const {
+  return layer_id_;
+}
+
+void Console::Newline() {
+  cursor_column_ = 0;
+  if (cursor_row_ < kRows - 1) {
+    ++cursor_row_;
+    return;
+  }
+
+  if (window_) {
+    Rectangle<int> move_src{{0, 16}, {8 * kColumns, 16 * (kRows - 1)}};
+    window_->Move({0, 0}, move_src);
+    FillRectangle(*writer_, {0, 16 * (kRows - 1)}, {8 * kColumns, 16}, bg_color_);
+  } else {
+    FillRectangle(*writer_, {0, 0}, {8 * kColumns, 16 * kRows}, bg_color_);
+    for (int row = 0; row < kRows - 1; ++row) {
+      memcpy(buffer_[row], buffer_[row + 1], kColumns + 1);
+      WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
+    }
+    memset(buffer_[kRows - 1], 0, kColumns + 1);
+  }
+}
+
+void Console::Refresh() {
+  FillRectangle(*writer_, {0, 0}, {8 * kColumns, 16 * kRows}, bg_color_);
+  for (int row = 0; row < kRows; ++row) {
+    WriteString(*writer_, Vector2D<int>{0, 16 * row}, buffer_[row], fg_color_);
+  }
+}
+
+Console* console;
+
+namespace {
+  char console_buf[sizeof(Console)];
+}
+
+void InitializeConsole() {
+  console = new(console_buf) Console{
+    kDesktopFGColor, kDesktopBGColor
+  };
+  console->SetWriter(screen_writer);
 }
