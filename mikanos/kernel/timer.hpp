d03fb77ed47163134b0689a3d591c711e640d71b
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-19 22:34:07 +0900

    11.13

--- mikanos/kernel/timer.hpp
+++ mikanos/kernel/timer.hpp
@@ -19,3 +19,14 @@ class TimerManager {
 extern TimerManager* timer_manager;
 
 void LAPICTimerOnInterrupt();
+
+class Timer {
+  public:
+    Timer(unsigned long timeout, int value);
+    unsigned long Timeout() const { return timeout_; }
+    int Value() const { return value_; }
+
+  private:
+    unsigned long timeout_;
+    int value_;
+};
