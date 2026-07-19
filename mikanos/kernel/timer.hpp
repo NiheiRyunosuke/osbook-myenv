d65ae1eeb536102d8d4b30bcfdc49d10f9981a11
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-19 22:58:08 +0900

    著者あわせ

--- mikanos/kernel/timer.hpp
+++ mikanos/kernel/timer.hpp
@@ -1,11 +1,30 @@
 #pragma once
 
 #include <cstdint>
+#include <queue>
+#include <vector>
+#include "message.hpp"
 
 void InitializeLAPICTimer();
 void StartLAPICTimer();
 uint32_t LAPICTimerElapsed();
 void StopLAPICTimer();
+
+class Timer {
+  public:
+    Timer(unsigned long timeout, int value);
+    unsigned long Timeout() const { return timeout_; }
+    int Value() const { return value_; }
+
+  private:
+    unsigned long timeout_;
+    int value_;
+};
+
+/** @brief タイマー優先度を比較する。タイムアウトが遠いほど優先度低。 */
+inline bool operator<(const Timer& lhs, const Timer& rhs) {
+  return lhs.Timeout() > rhs.Timeout();
+}
 
 class TimerManager {
   public:
@@ -23,15 +42,3 @@ class TimerManager {
 extern TimerManager* timer_manager;
 
 void LAPICTimerOnInterrupt();
-
-class Timer {
-  public:
-    Timer(unsigned long timeout, int value);
-    unsigned long Timeout() const { return timeout_; }
-    int Value() const { return value_; }
-
-  private:
-    unsigned long timeout_;
-    int value_;
-};
-
