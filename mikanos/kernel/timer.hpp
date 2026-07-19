28c0fdb3351b7cc903c72fabc4b1a2771795ff16
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-19 22:40:44 +0900

    11.14

--- mikanos/kernel/timer.hpp
+++ mikanos/kernel/timer.hpp
@@ -9,11 +9,15 @@ void StopLAPICTimer();
 
 class TimerManager {
   public:
+    TimerManager(std::deque<Message>& msg_queue);
+    void AddTimer(const Timer& timer);
     void Tick();
     unsigned long CurrentTick() const { return tick_; }
 
   private:
     volatile unsigned long tick_{0};
+    std::priority_queue<Timer> timers_{};
+    std::deque<Message>& msg_queue_;
 };
 
 extern TimerManager* timer_manager;
@@ -30,3 +34,4 @@ class Timer {
     unsigned long timeout_;
     int value_;
 };
+
