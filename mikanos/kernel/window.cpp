4a27dfc23c26ba93d3f61ca75e608664cca71c2d
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-26 19:37:46 +0900

    12.24

--- mikanos/kernel/window.cpp
+++ mikanos/kernel/window.cpp
@@ -21,14 +21,6 @@ namespace {
     ".$$$$$$$$$$$$$$@",
     "@@@@@@@@@@@@@@@@",
   };
-
-  constexpr PixelColor ToColor(uint32_t c) {
-    return {
-      static_cast<uint8_t>((c >> 16) & 0xff),
-      static_cast<uint8_t>((c >> 8) & 0xff),
-      static_cast<uint8_t>(c & 0xff)
-    };
-  }
 }
 
 Window::Window(int width, int height, PixelFormat shadow_format) : width_{width}, height_{height} {
