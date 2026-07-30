ece223096e65141c29fce75ac493c37fccd333e5
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-30 16:48:50 +0900

    13.18

--- mikanos/kernel/task.cpp
+++ mikanos/kernel/task.cpp
@@ -30,3 +30,24 @@ void InitializeTask() {
 
 Task::Task(uint64_t id) : id_{id} {
 }
+
+Task& Task::InitContext(TaskFunc* f, int64_t data) {
+  const size_t stack_size = kDefaultStackBytes / sizeof(stack_[0]);
+  stack_.resize(stack_size);
+  uint64_t stack_end = reinterpret_cast<uint64_t>(&stack_[stack_size]);
+
+  memset(&context_, 0, sizeof(context_));
+  context_.cr3 = GetCR3();
+  context_.rflags = 0x202;
+  context_.cs = kKernelCS;
+  context_.ss = kKernelSS;
+  context_.rsp = (stack_end & ~0xflu) - 8;
+  context_.rip = reinterpret_cast<uint64_t>(f);
+  context_.rdi = id_;
+  context_.rsi = data;
+
+  // MXCSR のすべての例外をマスクする
+  *reinterpret_cast<uint32_t*>(&context_.fxsave_area[24]) = 0x1f80;
+
+  return *this;
+}
