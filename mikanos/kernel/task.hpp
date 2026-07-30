7d8f403aef4eae698c95878b081ceba9009ae0bd
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-07-30 16:28:50 +0900

    13.16

--- mikanos/kernel/task.hpp
+++ mikanos/kernel/task.hpp
@@ -15,3 +15,18 @@ extern TaskContext task_b_ctx, task_a_ctx;
 
 void SwitchTask();
 void InitializeTask();
+
+using TaskFunc = void (uint64_t, int64_t);
+
+class Task {
+  public:
+    static const size_t kDefaultStackBytes = 4096;
+    Task(uint64_t id);
+    Task& initContext(TaskFunc* f, int64_t data);
+    TaskContext& Context();
+  
+  private:
+    uint64_t id_;
+    std::vector<uint64_t> stack_;
+    alignas(16) TaskContext context_;
+};
