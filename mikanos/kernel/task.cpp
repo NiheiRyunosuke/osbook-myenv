672beb38936051d3c62d89b0f072df15095df473
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-08-05 13:47:26 +0900

    14.18

--- mikanos/kernel/task.cpp
+++ mikanos/kernel/task.cpp
@@ -139,3 +139,14 @@ Error TaskManager::Wakeup(uint64_t id) {
   Wakeup(it->get());
   return MAKE_ERROR(Error::kSuccess);
 }
+
+Error TaskManager::SendMessage(uint64_t id, const Message& msg) {
+  auto it = std::find_if(tasks_.begin(), tasks_.end(),
+                        [id](const auto& t){ return t->ID() == id; });
+  if (it == tasks_.end()) {
+    return MAKE_ERROR(Error::kNoSuchTask);
+  }
+
+  (*it)->SendMessage(msg);
+  return MAKE_ERROR(Error::kSuccess);
+}
