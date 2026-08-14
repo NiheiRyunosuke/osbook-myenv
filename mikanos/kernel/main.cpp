ae48389d56632990e7adc57273f1453022d5fc8b
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-08-14 22:30:48 +0900

    15.19

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -81,8 +81,8 @@ int text_window_index;
 
 void DrawTextCursor(bool visible) {
   const auto color = visible ? ToColor(0) : ToColor(0xffffff);
-  const auto pos = Vector2D<int>{8 + 8*text_window_index, 24 + 5};
-  FillRectangle(*text_window->Writer(), pos, {7, 15}, color);
+  const auto pos = Vector2D<int>{4 + 8*text_window_index, 5};
+  FillRectangle(*text_window->InnerWriter(), pos, {7, 15}, color);
 }
 
 void InputTextWindow(char c) {
