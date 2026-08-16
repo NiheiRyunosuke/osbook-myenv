40368c037f870b9ae9306e6864c75481891a5e48
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-08-16 22:57:00 +0900

    15.23

--- mikanos/kernel/terminal.cpp
+++ mikanos/kernel/terminal.cpp
@@ -4,7 +4,7 @@ Terminal::Terminal() {
     kRows * 16 + 8 + ToplevelWindow::kMarginY,
     screen_config.pixel_format,
     "MikanTerm");
-  DrawTerminal(*window_->innerWriter(), {0, 0}, window->InnerSize());
+  DrawTerminal(*window_->InnerWriter(), {0, 0}, window_->InnerSize());
 
   layer_id_ = layer_manager->NewLayer()
     .SetWindow(window_)
@@ -12,3 +12,48 @@ Terminal::Terminal() {
     .ID();
 }
 
+void Terminal::BlinkCursor() {
+  cursor_visible_ = !cursor_visible_;
+  DrawCursor(cursor_visible_);
+}
+
+void Terminal::DrawCursor(bool visible) {
+  const auto color = visible ? ToColor(0xffffff) : ToColor(0);
+  const auto pos = Vector2D<int>{4 + 8*cursor_.x, 5 + 16*cursor_.y};
+  FillRectangle(*window_->InnerWriter(), pos, {7, 15}, color);
+}
+
+void TaskTerminal(uint64_t task_id, int64_t data) {
+  __asm__("cli");
+  Task& task = task_manager->CurrentTask();
+  Terminal* terminal = new Terminal;
+  layer_manager->Move(terminal->LayerID(), { 100, 200 });
+  active_layer->Activate(terminal->LayerID());
+  __asm__("sti");
+  while (true) {
+    __asm__("cli");
+    auto msg = task.ReceiveMessage();
+    if (!msg) {
+      task.Sleep();
+      __asm__("sti");
+      continue;
+    }
+
+    switch(msg->type) {
+    case Message::kTimerTimeout:
+      terminal->BlinkCursor();
+
+      {
+        Message msg{Message::kLayer, task_id};
+        msg.arg.layer.layer_id = terminal->LayerID();
+        msg.arg.layer.op = LayerOperation::Draw;
+        __asm__("cli");
+        task_manager->SendMessage(1, msg);
+        __asm__("sti");
+      }
+      break;
+    default:
+      break;
+    }
+  }
+}
