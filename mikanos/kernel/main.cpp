a96ca17e172ef23c1aadae1331af1c6e344b1cd3
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-08-18 02:28:42 +0900

    15.25

--- mikanos/kernel/main.cpp
+++ mikanos/kernel/main.cpp
@@ -28,6 +28,7 @@
 #include "acpi.hpp"
 #include "keyboard.hpp"
 #include "task.hpp"
+#include "terminal.hpp"
 
 int printk(const char* format, ...) {
   va_list ap;
@@ -201,6 +202,11 @@ extern "C" void KernelMainNewStack(
     .InitContext(TaskB, 45)
     .Wakeup()
     .ID();
+  
+  const uint64_t task_terminal_id = task_manager->NewTask()
+    .InitContext(TaskTerminal, 0)
+    .Wakeup()
+    .ID();
 
   usb::xhci::Initialize();
   InitializeKeyboard();
@@ -241,6 +247,10 @@ extern "C" void KernelMainNewStack(
         textbox_cursor_visible = !textbox_cursor_visible;
         DrawTextCursor(textbox_cursor_visible);
         layer_manager->Draw(text_window_layer_id);
+
+        __asm__("cli");
+        task_manager->SendMessage(task_terminal_id, *msg);
+        __asm__("sti");
       }
       break;
     case Message::kKeyPush:
