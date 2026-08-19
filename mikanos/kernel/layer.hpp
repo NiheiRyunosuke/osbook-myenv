9b73e2828a2939b80f7218bcc4962531efd61036
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-08-19 23:15:36 +0900

    15.34

--- mikanos/kernel/layer.hpp
+++ mikanos/kernel/layer.hpp
@@ -67,6 +67,7 @@ class LayerManager {
   void Draw(const Rectangle<int>& area) const;
   /** @brief 指定したレイヤーに設定されているウィンドウの描画領域内を再描画する。 */
   void Draw(unsigned int id) const;
+  void Draw(unsigned int id, Rectangle<int> area) const;
 
   /** @brief レイヤーの位置情報を指定された絶対座標へと更新する。再描画する。 */
   void Move(unsigned int id, Vector2D<int> new_pos);
