5450bfc8b32aadd4368cd1ecddf5942cc108c49c
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-08-21 02:40:03 +0900

    16.1

--- mikanos/kernel/terminal.hpp
+++ mikanos/kernel/terminal.hpp
@@ -21,6 +21,11 @@ class Terminal {
     Vector2D<int> cursor_{0, 0};
     bool cursor_visible_{false};
     void DrawCursor(bool visible);
+    Vector2D<int> CalcCursorPos() const;
+
+    int linebuf_index_{0};
+    std::array<char, kLineMax> linebuf_{};
+    void Scroll1();
 };
 
 void TaskTerminal(uint64_t task_id, int64_t data);
