3efeab8c7b7e7c1ac88350d7d8f37b71ef30dd5e
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-09-15 02:45:57 +0900

    16.12

--- mikanos/kernel/terminal.cpp
+++ mikanos/kernel/terminal.cpp
@@ -54,6 +54,7 @@ Rectangle<int> Terminal::InputKey(
       Scroll1();
     }
     ExecuteLine();
+    Print(">");
     draw_area.pos = ToplevelWindow::kTopLeftMargin;
     draw_area.size = window_->InnerSize();
   } else if (ascii == '\b') {
@@ -88,6 +89,25 @@ void Terminal::Scroll1() {
   window_->Move(ToplevelWindow::kTopLeftMargin + Vector2D<int>{4, 4}, move_src);
   FillRectangle(*window_->InnerWriter(),
                 {4, 4 + 16*cursor_.y}, {8*kColumns, 16}, {0, 0, 0});
+}
+
+void Terminal::ExecuteLine() {
+  char* command = &linebuf_[0];
+  char* first_arg = strchr(&linebuf_[0], ' ');
+  if (first_arg) {
+    *first_arg = 0;
+    ++first_arg;
+  }
+  if (strcmp(command, "echo") == 0) {
+    if (first_arg) {
+      Print(first_arg);
+    }
+    Print("\n");
+  } else if (command[0] != 0) {
+    Print("no such command: ");
+    Print(command);
+    Print("\n");
+  }
 }
 
 void Terminal::Print(const char* s) {
