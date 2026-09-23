7f664c64b64a8b10f384aa99348d30ecbfbcaba3
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-09-23 18:24:18 +0900

    17.8

--- mikanos/kernel/fat.cpp
+++ mikanos/kernel/fat.cpp
@@ -1,5 +1,13 @@
+#include "fat.hpp"
+
+#include <cstring>
+
+namespace fat {
+
 BPB* boot_volume_image;
 
 void initialize(void* volume_image) {
   boot_volume_image = reinterpret_cast<fat::BPB*>(volume_image);
 }
+
+} // namespace fat
