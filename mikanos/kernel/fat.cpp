6ee3276cda4e8bb8152cf7bb46048e043851c595
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-09-23 19:22:00 +0900

    17.12

--- mikanos/kernel/fat.cpp
+++ mikanos/kernel/fat.cpp
@@ -6,8 +6,17 @@ namespace fat {
 
 BPB* boot_volume_image;
 
-void initialize(void* volume_image) {
+void Initialize(void* volume_image) {
   boot_volume_image = reinterpret_cast<fat::BPB*>(volume_image);
+}
+
+uintptr_t GetClusterAddr(unsigned long cluster) {
+  unsigned long sector_num =
+    boot_volume_image->reserved_sector_count +
+    boot_volume_image->num_fats * boot_volume_image->fat_size_32 +
+    (cluster - 2) * boot_volume_image->sectors_per_cluster;
+  uintptr_t offset = sector_num * boot_volume_image->bytes_per_sector;
+  return reinterpret_cast<uintptr_t>(boot_volume_image) + offset;
 }
 
 } // namespace fat
