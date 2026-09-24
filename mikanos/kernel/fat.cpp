8bc6d1273bb0a97f7bde34fc9e9d6d098c2625b8
Ryunosuke Nihei <soccerrobot7331@gmail.com>
2026-09-24 20:46:24 +0900

    17.13

--- mikanos/kernel/fat.cpp
+++ mikanos/kernel/fat.cpp
@@ -19,4 +19,17 @@ uintptr_t GetClusterAddr(unsigned long cluster) {
   return reinterpret_cast<uintptr_t>(boot_volume_image) + offset;
 }
 
+void ReadName(const DirectoryEntry& entry, char* base, char* ext) {
+  memcpy(base, &entry.name[0], 8);
+  base[8] = 0;
+  for (int i = 7; i >= 0 && base[i] == 0x20; --i) {
+    base[i] = 0;
+  }
+  memcpy(ext, &entry.name[8], 3);
+  ext[3] = 0;
+  for (int i = 2; i >= 0 && ext[i] == 0x20; --i) {
+    ext[i] = 0;
+  }
+}
+
 } // namespace fat
